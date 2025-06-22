c1ec6972a94c5d387c584e23a65490f85be75051
bezo <bezo16@protonmail.ch>
2025-06-22 18:23:12 +0200

    feat: added original eslint format plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import stylistic from "@stylistic/eslint-plugin"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:prettier/recommended",
-  ),
-];
+const eslintConfig = [stylistic.configs.customize({
+  indent: 2,
+  quotes: "double",
+  semi: false,
+  jsx: true,
+  // Add other stylistic options as needed
+}), ...compat.extends(
+  "next/core-web-vitals",
+  "next/typescript",
+), {
+  plugins: {
+    "@stylistic": stylistic,
+  },
+}]
 
-export default eslintConfig;
+export default eslintConfig
