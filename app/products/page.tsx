a6910ee481c06497d9386767c817af3482a3e73a
bezo <bezo16@protonmail.ch>
2025-06-25 08:58:11 +0200

    build: ProductsList component
    fixed rerendering of whole page which was very bad for ux + added modularity with this

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,19 +1,12 @@
 "use client"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import Link from "next/link"
-import useProducts, { filterOptions } from "@/hooks/useProducts"
+
+import { filterOptions } from "@/hooks/useProducts"
 import { useState } from "react"
 import RadioCards from "@/components/RadioCards"
 import { Select } from "@radix-ui/react-select"
 import { SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 import useCategories from "@/hooks/useCategories"
+import ProductsList from "@/components/lists/ProductsList"
 
 const options = [
   {
@@ -33,28 +26,6 @@ const options = [
 const ProductsPage = () => {
   const [filters, setFilters] = useState<filterOptions>({ limit: 15, page: 1, category: "all", sort: "none" })
   const { data: categories } = useCategories()
-  const { data: productsData, error, isLoading } = useProducts(filters)
-
-  if (isLoading) {
-    return <div className="container mx-auto py-10">Načítavanie produktov...</div>
-  }
-
-  if (productsData && productsData.products.length === 0) {
-    return <div className="container mx-auto py-10">Žiadne produkty na zobrazenie.</div>
-  }
-
-  if (error) {
-    return (
-      <div className="container mx-auto py-10">
-        Chyba pri načítavaní produktov:
-        {error.message}
-      </div>
-    )
-  }
-
-  if (!productsData) {
-    return <div className="container mx-auto py-10">Žiadne produkty na zobrazenie.</div>
-  }
 
   return (
     <div className="container mx-auto py-10">
@@ -64,6 +35,7 @@ const ProductsPage = () => {
           <RadioCards options={options} setFilters={setFilters} filters={filters} />
           <Select value={filters.category} onValueChange={value => setFilters(prev => ({ ...prev, category: value }))}>
             <SelectTrigger className="w-[180px]">
+              <h2>category:</h2>
               <SelectValue placeholder="Select a Category" />
             </SelectTrigger>
             <SelectContent>
@@ -78,66 +50,25 @@ const ProductsPage = () => {
               </SelectGroup>
             </SelectContent>
           </Select>
-          <Select value={filters.category} onValueChange={value => setFilters(prev => ({ ...prev, sort: (value as "price-asc" | "price-desc" | "none") }))}>
+          <Select value={filters.sort} onValueChange={value => setFilters(prev => ({ ...prev, sort: (value as "price-asc" | "price-desc" | "none") }))}>
             <SelectTrigger className="w-[180px]">
+              <h2>sort by:</h2>
               <SelectValue placeholder="Select a Sorting" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
                 <SelectLabel>Sort</SelectLabel>
                 <SelectItem value="none">none</SelectItem>
-                <SelectItem value="price-asc">price ascending</SelectItem>
-                <SelectItem value="price-desc">price descending</SelectItem>
+                <SelectItem value="price-asc">price asc</SelectItem>
+                <SelectItem value="price-desc">price desc</SelectItem>
               </SelectGroup>
             </SelectContent>
           </Select>
         </div>
       </header>
-      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {productsData?.products.map(product => (
-          <Card key={product.id}>
-            <CardHeader>
-              <CardTitle>{product.title}</CardTitle>
-              <CardDescription>{product.description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Link href={`/products/${product.id}`} className="w-full">
-                <Button size="lg" variant="default">Zobraziť detail</Button>
-                <div className="mt-4 text-lg font-semibold">
-                  <h2>
-                    Cena:
-                    {" "}
-                    {product.price.toLocaleString("sk-SK")}
-                    {" "}
-                    €
-                  </h2>
-                </div>
-              </Link>
-            </CardContent>
-          </Card>
-        ))}
+      <main>
+        <ProductsList filters={filters} setFilters={setFilters} />
       </main>
-      <footer className="mt-10">
-        <div className="flex justify-between items-center">
-          <Button
-            disabled={filters.page <= 1}
-            onClick={() => setFilters(prev => ({ ...prev, page: prev.page - 1 }))}
-          >
-            Predchádzajúca
-          </Button>
-          <span>
-            Strana
-            {" "}
-            {filters.page}
-          </span>
-          <Button
-            disabled={productsData.products.length < filters.limit}
-            onClick={() => setFilters(prev => ({ ...prev, page: prev.page + 1 }))}
-          >
-            Ďalšia
-          </Button>
-        </div>
-      </footer>
     </div>
   )
 }
---
+++ components/lists/ProductsList.tsx
@@ -0,0 +1,93 @@
+import { Dispatch, FC, SetStateAction } from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Link from "next/link"
+import useProducts, { filterOptions } from "@/hooks/useProducts"
+
+type Props = {
+  filters: filterOptions
+  setFilters: Dispatch<SetStateAction<filterOptions>>
+}
+
+const ProductsList: FC<Props> = ({ filters, setFilters }) => {
+  const { data: productsData, error, isLoading } = useProducts(filters)
+
+  if (isLoading) {
+    return <div className="container mx-auto py-10">Načítavanie produktov...</div>
+  }
+
+  if (productsData && productsData.products.length === 0) {
+    return <div className="container mx-auto py-10">Žiadne produkty na zobrazenie.</div>
+  }
+
+  if (error) {
+    return (
+      <div className="container mx-auto py-10">
+        Chyba pri načítavaní produktov:
+        {error.message}
+      </div>
+    )
+  }
+
+  if (!productsData) {
+    return <div className="container mx-auto py-10">Žiadne produkty na zobrazenie.</div>
+  }
+
+  return (
+    <div>
+      <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {productsData?.products.map(product => (
+          <Card key={product.id}>
+            <CardHeader>
+              <CardTitle>{product.title}</CardTitle>
+              <CardDescription>{product.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Link href={`/products/${product.id}`} className="w-full">
+                <Button size="lg" variant="default">Zobraziť detail</Button>
+                <div className="mt-4 text-lg font-semibold">
+                  <h2>
+                    Cena:
+                    {" "}
+                    {product.price.toLocaleString("sk-SK")}
+                    {" "}
+                    €
+                  </h2>
+                </div>
+              </Link>
+            </CardContent>
+          </Card>
+        ))}
+      </main>
+      <footer className="mt-10">
+        <div className="flex justify-between items-center">
+          <Button
+            disabled={filters.page <= 1}
+            onClick={() => setFilters(prev => ({ ...prev, page: prev.page - 1 }))}
+          >
+            Predchádzajúca
+          </Button>
+          <span>
+            Strana
+            {" "}
+            {filters.page}
+          </span>
+          <Button
+            disabled={productsData.products.length < filters.limit}
+            onClick={() => setFilters(prev => ({ ...prev, page: prev.page + 1 }))}
+          >
+            Ďalšia
+          </Button>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default ProductsList
